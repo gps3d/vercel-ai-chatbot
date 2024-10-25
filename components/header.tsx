ee42f301dd9d0468fa0232e3dfe2c7717238412b
Shashi Nittur <66386365+gps3d@users.noreply.github.com>
2024-10-25 19:35:09 -0400

    Removed GH/VCL links

--- components/header.tsx
+++ components/header.tsx
@@ -1,27 +1,32 @@
 import * as React from 'react'
 import Link from 'next/link'
+import { headers } from 'next/headers'
 
-import { cn } from '@/lib/utils'
-import { auth } from '@/auth'
-import { clearChats } from '@/app/actions'
-import { Button, buttonVariants } from '@/components/ui/button'
-import { Sidebar } from '@/components/sidebar'
-import { SidebarList } from '@/components/sidebar-list'
+import { cn } from 'lib/utils'
+import { auth } from 'auth'
+import { clearChats } from 'app/actions'
+import { Button, buttonVariants } from 'components/ui/button'
+import { Sidebar } from 'components/sidebar'
+import { SidebarList } from 'components/sidebar-list'
 import {
   IconGitHub,
   IconNextChat,
   IconSeparator,
   IconVercel
-} from '@/components/ui/icons'
-import { SidebarFooter } from '@/components/sidebar-footer'
-import { ThemeToggle } from '@/components/theme-toggle'
-import { ClearHistory } from '@/components/clear-history'
-import { UserMenu } from '@/components/user-menu'
+} from 'components/ui/icons'
+import { SidebarFooter } from 'components/sidebar-footer'
+import { ThemeToggle } from 'components/theme-toggle'
+import { ClearHistory } from 'components/clear-history'
+import { UserMenu } from 'components/user-menu'
 import { cookies } from 'next/headers'
 
 export async function Header() {
   const cookieStore = cookies()
   const session = await auth({ cookieStore })
+  const headersList = headers()
+  const pathname = headersList.get('x-invoke-path') || ''
+  const isAuthPage = pathname === '/sign-up' || pathname === '/sign-in'
+
   return (
     <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl">
       <div className="flex items-center">
@@ -53,26 +58,28 @@ export async function Header() {
           )}
         </div>
       </div>
-      <div className="flex items-center justify-end space-x-2">
-        <a
-          target="_blank"
-          href="https://github.com/thorwebdev/vercel-ai-chatbot"
-          rel="noopener noreferrer"
-          className={cn(buttonVariants({ variant: 'outline' }))}
-        >
-          <IconGitHub />
-          <span className="ml-2 hidden md:flex">GitHub</span>
-        </a>
-        <a
-          href="https://github.com/thorwebdev/vercel-ai-chatbot"
-          target="_blank"
-          className={cn(buttonVariants())}
-        >
-          <IconVercel className="mr-2" />
-          <span className="hidden sm:block">Deploy to Vercel</span>
-          <span className="sm:hidden">Deploy</span>
-        </a>
-      </div>
+      {!isAuthPage && (
+        <div className="flex items-center justify-end space-x-2">
+          <a
+            target="_blank"
+            href="https://github.com/thorwebdev/vercel-ai-chatbot"
+            rel="noopener noreferrer"
+            className={cn(buttonVariants({ variant: 'outline' }))}
+          >
+            <IconGitHub />
+            <span className="ml-2 hidden md:flex">GitHub</span>
+          </a>
+          <a
+            href="https://github.com/thorwebdev/vercel-ai-chatbot"
+            target="_blank"
+            className={cn(buttonVariants())}
+          >
+            <IconVercel className="mr-2" />
+            <span className="hidden sm:block">Deploy to Vercel</span>
+            <span className="sm:hidden">Deploy</span>
+          </a>
+        </div>
+      )}
     </header>
   )
 }
